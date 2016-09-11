03407d23c5b98538b3cd482ba365338460e56342
Taylor McClure <Taylor McClure>
2016-09-10 21:37:53 -0700

    Signed-off-by: Taylor McClure <Taylor McClure>

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -730,7 +730,7 @@ func main() {
 
 	// Create a discord session
 	log.Info("Starting discord session...")
-	discord, err = discordgo.New(TOKEN)
+	discord, err = discordgo.New("Bot " + *Token)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
